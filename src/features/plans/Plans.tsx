657eec7165cdb92a56181dd27d237de38d19c514
Sam Bennett <sambennettdev@gmail.como>
2024-10-15 16:05:20 -0600

    test runner

--- src/features/plans/Plans.tsx
+++ src/features/plans/Plans.tsx
@@ -133,7 +133,7 @@ const PricingCard = ({
 const CheckItem = ({ text }: { text: string }) => (
   <div className="flex gap-2">
     <CheckCircle2 size={18} className="my-auto text-green-400" />
-    <p className="pt-0.5 text-zinc-700 dark:text-zinc-300 text-sm">{text}</p>
+    <p className="pt-0.5 text-foreground text-sm">{text}</p>
   </div>
 );
 
